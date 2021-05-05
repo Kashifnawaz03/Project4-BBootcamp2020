8952e22805ab5fa1f73cce7417f737f36e28290d
kashifnawaz <kashi030@hotmail.com>
2021-05-06 01:08:45 +0500

    No idea

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import { Navbar } from './components/Navbar';
+//import { Navbar } from './components/Navbar';
 import { Main } from './components/Main';
 
 function App() {
